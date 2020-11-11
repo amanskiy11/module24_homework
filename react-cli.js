3a082ee755dd90549903145cf4b44edce7f70ff8
Aman <a.alapaev@gmail.com>
2020-11-11 13:09:08 +0600

    Initial commit

---
+++ react-cli.js
@@ -0,0 +1,21 @@
+const yargs = require('yargs');
+const fs = require('fs/promises');
+const path = require('path');
+const camelCase = require('camelcase');
+
+const argv = yargs
+    .command(['$0 <filename>'], 'Creates react component', {}, (yargs) => {
+        const filepath = path.resolve(`${camelCase(yargs.filename)}.js`);
+        const res = `import React from 'react';
+
+export const ${camelCase(yargs.filename)} = (props) => {
+  return (
+    <div>${yargs.filename} works</div>
+  );
+}
+`
+        fs.writeFile(filepath, res)
+            .then(() => console.log('Success'))
+            .catch(e => console.error(e))
+    })
+    .argv
